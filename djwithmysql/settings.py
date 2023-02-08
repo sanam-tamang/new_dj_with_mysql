aa8460296c81692f6fd065de9b2533ddb13f9ff4
sanam-tamang <snm.tmg7@gmail.com>
2023-02-08 11:13:40 +0545

    added secure proxy and session cookie

--- djwithmysql/settings.py
+++ djwithmysql/settings.py
@@ -132,3 +132,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
