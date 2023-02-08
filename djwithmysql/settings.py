4f35ced85287efc25b5d5b732ebfacc589c9123a
sanam-tamang <snm.tmg7@gmail.com>
2023-02-08 14:41:05 +0545

    added static directories and adding url static files

--- djwithmysql/settings.py
+++ djwithmysql/settings.py
@@ -127,9 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATIC_ROOT = os.path.normpath(os.path.join(os.path.dirname(BASE_DIR), 'static'))
-STATICFILES_DIRS = []
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static/')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
