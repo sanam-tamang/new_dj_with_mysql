0bc3be9e84eabab83b7a89fa0fad2bc7501746ee
sanam-tamang <snm.tmg7@gmail.com>
2023-02-08 12:02:16 +0545

    git remove static and added gitgnore

--- djwithmysql/settings.py
+++ djwithmysql/settings.py
@@ -125,16 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-
-STATIC_URL= "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-
-
-
-
-MEDIA_ROOT= os.path.join(BASE_DIR, "media")
-MEDIA_URL="/media/"
+STATIC_ROOT = os.path.normpath(os.path.join(os.path.dirname(BASE_DIR), 'static'))
+STATICFILES_DIRS = []
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
